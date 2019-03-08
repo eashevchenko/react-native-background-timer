9d21a4973673881deb2aece15ed11741d920314f
Eugene Shevchenko <eugene.shevchenko@swanlogic.com>
2019-03-08 16:56:55 +0200

    iOS execute task for a long time

--- ios/RNBackgroundTimer.m
+++ ios/RNBackgroundTimer.m
@@ -69,13 +69,17 @@ RCT_EXPORT_METHOD(setTimeout:(int)timeoutId
         [[UIApplication sharedApplication] endBackgroundTask:task];
     }];
 
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul), ^(void) {
+        
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, timeout * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
         if ([self bridge] != nil) {
             [self sendEventWithName:@"backgroundTimer.timeout" body:[NSNumber numberWithInt:timeoutId]];
+              resolve([NSNumber numberWithBool:YES]);
+              [[UIApplication sharedApplication] endBackgroundTask:task];
         }
-        [[UIApplication sharedApplication] endBackgroundTask:task];
     });
-    resolve([NSNumber numberWithBool:YES]);
+});
+    
 }
 
 /*
